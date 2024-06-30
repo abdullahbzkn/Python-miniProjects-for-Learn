50e753e4bd13c1955d801c051b8a3e0d9cede61b
Abdullah Buzkan <abdullahbuzkan2023@gmail.com>
2024-06-30 12:36:34 +0300

    secretNotesApp updated

--- SecretNotesApp/main.py
+++ SecretNotesApp/main.py
@@ -1,49 +1,69 @@
 import tkinter as tk
 from tkinter import messagebox
 from PIL import Image, ImageTk
-import pybase64
+import base64
 
-window = tk.Tk()
-window.title('Secret Notes')
-window.config(padx=80, pady=20)
+
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
 
 
 def save_and_encrypt():
     key = my_entry3.get()
+    note = my_entry2.get(1.0, tk.END)
+    note_title = my_entry1.get()
 
     if len(my_entry1.get()) == 0 or len(my_entry2.get(1.0,tk.END)) == 0 or len(my_entry3.get()) == 0:
         messagebox.showinfo('Error!', 'Please enter all')
     else:
-        my_entry3.delete(0, tk.END)
-        my_entry3.focus()
-        if key == "tkinter":
-            file_path = open(file="secret notes/secret_notes.txt", mode='a')
-            note_title = my_entry1.get()
+
+        message_encrypted = encode(key, note)
+
+        try:
+            with open("secret notes/secret_notes.txt", "a") as data_file:
+                data_file.write(f'\n{note_title}\n{message_encrypted}')
+        except FileNotFoundError:
+            with open("secret notes/secret_notes.txt", "w") as data_file:
+                data_file.write(f'\n{note_title}\n{message_encrypted}')
+        finally:
             my_entry1.delete(0, tk.END)
-            file_path.write(note_title)
-            file_path.write("\n")
-            note = my_entry2.get(1.0, tk.END)
-            my_entry2.delete(1.0, tk.END)
-            note = note.encode("ascii")
-            note = pybase64.b64encode(note)
-            note = note.decode("ascii")
-            file_path.write(note)
-            file_path.write("\n")
-            file_path.close()
+            my_entry3.delete(0, tk.END)
+            my_entry2.delete("1.0", tk.END)
 
 
 def decrypt():
+    message_encrypted = my_entry2.get("1.0", tk.END)
     key = my_entry3.get()
-    my_entry3.delete(0, tk.END)
 
-    if key == "tkinter":
-        note = my_entry2.get(1.0, tk.END)
-        note = note.encode("ascii")
-        note = pybase64.b64decode(note)
-        note = note.decode("ascii")
-        my_entry2.delete(1.0, tk.END)
-        my_entry2.insert(1.0, note)
+    if len(message_encrypted) == 0 or len(key) == 0:
+        messagebox.showinfo(title="Error!", message="Please enter all information.")
+    else:
+        try:
+            decrypted_message = decode(key, message_encrypted)
+            my_entry2.delete("1.0", tk.END)
+            my_entry2.insert("1.0", decrypted_message)
+        except:
+            messagebox.showinfo(title="Error!", message="Please make sure of encrypted info.")
 
+
+window = tk.Tk()
+window.title('Secret Notes')
+window.config(padx=80, pady=20)
 
 image = Image.open("secret notes/logo.png")
 resized_img = image.resize((200,200))
